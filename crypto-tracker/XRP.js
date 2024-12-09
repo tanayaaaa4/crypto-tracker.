f6cacfcd4c05a9a9307930db07b64c1e7f69ee5f
tanayaaaa4 <145769135+tanayaaaa4@users.noreply.github.com>
2024-12-10 01:37:49 +0700

    Delete crypto-tracker/XRP.js

--- crypto-tracker/XRP.js
+++
@@ -1,6 +0,0 @@
-const apiUrl = 'https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT';
-
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.error('Error:', error));
